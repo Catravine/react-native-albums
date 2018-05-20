de79145a317ec21eb5169c6580e2c9db72c567d7
Caroline Courtney <caroline@carolinecourtney.com>
2018-05-20 18:46:28 -0400

    Initial Commit: it runs, with text on the screen

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+// Import libraries for making a registerComponent
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make a component
+const Header = () => (
+    <Text>Albums!</Text>
+);
+
+// Make the component available to other parts of the app.
+export default Header;
